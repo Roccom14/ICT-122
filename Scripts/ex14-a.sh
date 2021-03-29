7e3da5570ca95a7809120c8cf1802171fc684fb0
Rocco <roccoronzano@hotmail.com>
2021-03-29 09:50:21 +0200

    add modif

---
+++ Scripts/ex14-a.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# ###
+# date
+# Rocco Ronzano
+# Education - Apprentissage du shell script
+# fonction du shell script
+# ###
+echo "Début du script"
+
+echo "Fin du script"
+# ###
+# end of shell script
+# ###
