d440ea33846e4fc7d874bd190a939c9e0b906213
Roccom14 <roccoronzan@hotmail.com>
2021-03-31 14:33:25 +0200

    add modif

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # ICT-122
+Répertoire des exercices de shell script du module ICT-122
+
+_by Roccom14_
--- selecteur.sh
+++ selecteur.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+# ###
+# date
+# Rocco Ronzano
+# Education - Apprentissage du shell script
+# Sélecteur d'exercices en shell script
+# ###
 
+# ###
+# end of shell script
+# ###
