bfb85806b6b7dd52590176bc06acebb23fdf6453
Rocco Ronzano <40248252+Roccom14@users.noreply.github.com>
2021-05-31 09:02:55 +0200

    [ADD] Ex19-a

--- Scripts/ex19-a.sh
+++ Scripts/ex19-a.sh
@@ -10,7 +10,33 @@ echo "----- Début du script -----"
 echo ""
 echo ""
 
+echo "Calcul de la surface d'un rectangle."
 
+while [[ "$recommencer" != "Non" ]]
+do
+    largeur=0
+    hauteur=0
+    
+    read -p "Entrez la largeur : " largeur
+    read -p "Entrez la hauteur : " hauteur
+
+    surface=$( echo "$largeur*$hauteur" | bc )
+    
+    echo "La surface est de : $surface"
+    PS3="Réponse : "
+    select recommencer in Oui Non
+    do
+        case $recommencer in
+            Oui )
+                break;;
+            Non )
+                echo "Bye !"
+                break;;
+            * )
+                echo "Valeur inconnue";;
+        esac
+    done
+done
 
 echo ""
 echo ""
